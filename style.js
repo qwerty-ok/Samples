2570a5d3c62f64384f6223ae679a773b10d66640
qwerty-ok <VALUEPACKSTORECOM@GMAIL.COM>
2025-01-02 17:13:51 +0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,14 @@
+function openNav(){ document.getElementById("Nav").style.width = "260px"
+    }
+    function closeNav(){   document.getElementById("Nav").style.width = "0px"
+    }
+    /* end of styling in index.html file */
+    
+   function myFunction(){
+       let e = document.getElementById("email").value;
+       if(e ===""){
+           alert("Please Enter Your Email");
+       }
+       else{alert("Submitted Successfully")}
+   }
+   /* end of styling in subscribe.html file*/
